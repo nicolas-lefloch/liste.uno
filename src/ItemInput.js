4877ed85c309b8c144b4b4e9cac6b1b3c3165987
Noan Cloarec <noan.cloarec@gmail.com>
2020-11-04 15:40:22 +0100

    voice input for list items

--- src/ItemInput.js
+++ src/ItemInput.js
@@ -1,23 +1,48 @@
-import React , {useState}from 'react';
-
+import React, { useState } from 'react';
+import {VoiceRecorder} from './voice-recorder.service'
 const ItemInput = (props) => {
     const [value, setValue] = useState('')
+    const [listening, setListening] = useState(false)
+
 
     const handleKeyPress = (event) => {
         if (event.code === "Enter") {
-            const newItem = {id : Math.random(), value: event.target.value}
+            const newItem = { id: Math.random(), value: event.target.value }
             props.onItemOutput(newItem);
             setValue('');
 
         }
     }
 
-    return <input type="text"
-          placeholder={props.placeholder}
-          value={value}
-          onInput={event => setValue(event.target.value)}
-          onKeyPress={handleKeyPress}
-    />;
+    const startRecording = () => {
+        setListening(true)
+        VoiceRecorder.recordShoppingList().then(
+            items => items.map(item => 
+                props.onItemOutput({id : Math.random(), value : item})
+            )
+        ).finally(()=> setListening(false))
+    }
+
+    const stopRecording = () => {
+        setListening(false)
+        VoiceRecorder.stopVoiceRecognition();
+    }
+
+    return (
+        <>
+            <input type="text"
+                placeholder={props.placeholder}
+                value={value}
+                onInput={event => setValue(event.target.value)}
+                onKeyPress={handleKeyPress}
+            />
+            <button
+                style={{ width: '120px', color: 'black' }}
+                onClick={listening ? stopRecording : startRecording}>
+                {listening ? 'Listening...' : 'Audio'}
+            </button>
+        </>
+    );
 
 }
 export default ItemInput;
---
+++ src/voice-recorder.service.js
@@ -0,0 +1,32 @@
+const SpeechRecognition = window.webkitSpeechRecognition
+const recognition = new SpeechRecognition()
+recognition.lang = 'fr-FR'
+export class VoiceRecorder {
+    static recordShoppingList() {
+        console.log(recognition);
+
+        return new Promise((resolve, reject) => {
+            recognition.onresult = event => {
+                const transcript = event.results[0][0].transcript
+                resolve(VoiceRecorder.toShoppingList(transcript))
+            }
+            recognition.onend = reject // will be called only if resolve did not happen (i.e. no result was outputed)
+            recognition.start()
+        })
+    }
+
+    static stopVoiceRecognition() {
+        recognition.onend = () => { }
+        recognition.stop()
+    }
+
+    static toShoppingList(transcript) {
+        console.log(transcript)
+        const res = transcript.split(/puis|et/)
+            .map(s => s.trim())
+            .map(item => item.replace(/^des /, ''))
+            .map(s => s.charAt(0).toUpperCase()+s.slice(1))
+        console.log(res);
+        return res
+    }
+}
