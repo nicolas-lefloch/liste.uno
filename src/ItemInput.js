942d47d57632170709f000ef3420e64d9b814348
Noan Cloarec <noan.cloarec@gmail.com>
2020-11-04 16:03:24 +0100

    switched input to form

--- src/ItemInput.js
+++ src/ItemInput.js
@@ -1,26 +1,24 @@
 import React, { useState } from 'react';
-import {VoiceRecorder} from './voice-recorder.service'
+import { VoiceRecorder } from './voice-recorder.service'
 const ItemInput = (props) => {
     const [value, setValue] = useState('')
     const [listening, setListening] = useState(false)
 
 
-    const handleKeyPress = (event) => {
-        if (event.code === "Enter") {
-            const newItem = { id: Math.random(), value: event.target.value }
-            props.onItemOutput(newItem);
-            setValue('');
-
-        }
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const newItem = { id: Math.random(), value: value }
+        props.onItemOutput(newItem);
+        setValue('');
     }
 
     const startRecording = () => {
         setListening(true)
         VoiceRecorder.recordShoppingList().then(
-            items => items.map(item => 
-                props.onItemOutput({id : Math.random(), value : item})
+            items => items.map(item =>
+                props.onItemOutput({ id: Math.random(), value: item })
             )
-        ).finally(()=> setListening(false))
+        ).finally(() => setListening(false))
     }
 
     const stopRecording = () => {
@@ -30,12 +28,13 @@ const ItemInput = (props) => {
 
     return (
         <>
-            <input type="text"
-                placeholder={props.placeholder}
-                value={value}
-                onInput={event => setValue(event.target.value)}
-                onKeyPress={handleKeyPress}
-            />
+            <form onSubmit={onSubmit}>
+                <input type="text"
+                    placeholder={props.placeholder}
+                    value={value}
+                    onInput={event => setValue(event.target.value)}
+                />
+            </form>
             <button
                 style={{ width: '120px', color: 'black' }}
                 onClick={listening ? stopRecording : startRecording}>
