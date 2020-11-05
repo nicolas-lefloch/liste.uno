08a324fd4928af139d57b26673dafb39586a6faf
Noan Cloarec <noan.cloarec@gmail.com>
2020-11-05 10:53:31 +0100

    initialized typescript and eslint

---
+++ src/Item.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const Item = (props) => {
+  const [name, setName] = useState(props.name);
+  const [editable, setEditable] = useState(false);
+
+  const handleKeyPress = (event) => {
+    if (event.code === 'Enter') {
+      setEditable(false);
+    }
+  };
+
+  let el;
+  if (!editable) {
+    el = (
+      <li className="button-container" onDoubleClick={() => setEditable(!editable)}>
+        {name}
+        <button type="button" className="negative" onClick={props.onDelete}>X</button>
+      </li>
+    );
+  } else {
+    el = (
+      <li className="button-container">
+        <input
+          className="item"
+          onKeyPress={handleKeyPress}
+          onChange={(event) => setName(event.target.value)}
+          // autoFocus
+          type="text"
+          value={name}
+        />
+        <button type="button" className="positive" onClick={() => setEditable(false)}>V</button>
+
+      </li>
+    );
+  }
+
+  return el;
+};
+
+export default Item;
