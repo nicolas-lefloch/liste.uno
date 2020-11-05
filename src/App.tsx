956483b46dec44328d56cd1b183474ecc29ba6de
Noan Cloarec <noan.cloarec@gmail.com>
2020-11-05 12:56:15 +0100

    switched all files to typescript linted

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import ItemList from './ItemList';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1> GEOSHOP</h1>
+                <ItemList />
+            </header>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/ItemList.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import ItemInput from './ItemInput';
+import ItemRow from './ItemRow';
+
+const ItemList = () => {
+    const [list, setList] = useState([{ id: 1, value: 'Banane' },
+        { id: 2, value: 'Pomme' },
+        { id: 3, value: 'Café' },
+        { id: 4, value: 'Beurre' }]);
+
+    const addItemToList = (item) => {
+        setList([...list, item]);
+    };
+
+    const deleteItem = (id) => {
+        setList(list.filter((e) => e.id !== id));
+    };
+
+    const itemList = list.map(
+        (item) => <ItemRow key={item.id} name={item.value} onDelete={() => deleteItem(item.id)} />,
+    );
+
+    return (
+        <div className="itemList">
+            <ItemInput placeholder="Ajouter un item" onItemOutput={addItemToList} />
+            <ol>
+                {itemList}
+            </ol>
+        </div>
+    );
+};
+
+export default ItemList;
