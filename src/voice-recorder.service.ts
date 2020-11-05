a78d98cde7a917fb95c53ee78b6252ca11c41328
Noan Cloarec <noan.cloarec@gmail.com>
2020-11-05 12:59:50 +0100

    learning git

--- src/voice-recorder.service.ts
+++ src/voice-recorder.service.ts
@@ -1,34 +1,34 @@
-const SpeechRecognition = window.SpeechRecognition || (window as any).webkitSpeechRecognition
-const recognition = SpeechRecognition ? new SpeechRecognition() : undefined
+const SpeechRecognition = window.SpeechRecognition || (window as any).webkitSpeechRecognition;
+const recognition = SpeechRecognition ? new SpeechRecognition() : undefined;
 
 if (recognition) {
-    recognition.lang = 'fr-FR'
+    recognition.lang = 'fr-FR';
 }
-export class VoiceRecorder {
+export default class VoiceRecorder {
     static voiceRecognitionIsSupported = !!recognition;
 
     static recordShoppingList(): Promise<string[]> {
-
         return new Promise((resolve, reject) => {
-            recognition.onresult = event => {
-                const transcript = event.results[0][0].transcript
-                resolve(VoiceRecorder.toShoppingList(transcript))
-            }
-            recognition.onend = reject // will be called only if resolve did not happen (i.e. no result was outputed)
-            recognition.start()
-        })
+            recognition.onresult = (event) => {
+                const { transcript } = event.results[0][0];
+                resolve(VoiceRecorder.toShoppingList(transcript));
+            };
+            // will be called only if resolve did not happen (i.e. no result was outputed)
+            recognition.onend = reject;
+            recognition.start();
+        });
     }
 
     static stopVoiceRecognition() {
-        recognition.onend = () => { }
-        recognition.stop()
+        recognition.onend = () => { };
+        recognition.stop();
     }
 
     static toShoppingList(transcript: string) {
         const res = transcript.split(/ puis | et /)
-            .map(s => s.trim())
-            .map(item => item.replace(/^des |^du /, ''))
-            .map(s => s.charAt(0).toUpperCase() + s.slice(1))
-        return res
+            .map((s) => s.trim())
+            .map((item) => item.replace(/^des |^du /, ''))
+            .map((s) => s.charAt(0).toUpperCase() + s.slice(1));
+        return res;
     }
 }
